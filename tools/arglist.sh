d5ef2cfb6f5d24a25f2eeac398af0853119b70ef
sitemapxml <viktor.pavlovic@protonmail.com>
2022-09-16 02:19:48 +0200

    Added varlist.sh
     - varlist.sh has been added. This script is used to generate a list of available configuration variables.
     - slight modifications to arglist.sh

--- tools/arglist.sh
+++ tools/arglist.sh
@@ -15,9 +15,9 @@ cat << EOT
   Generate list of available arguments.
   usage $me [-hlb]
   OPTIONS
-    -l    [list] make a list of arguments
-    -b    [bare] remove minus sign before argument names
-    -h    show this message
+    -l  [list]  make a list of arguments
+    -b  [bare]  remove minus sign before argument names
+    -h  [help]  show this message
 EOT
 }
 
@@ -27,15 +27,13 @@ list () {
 
 while getopts "hlb" option; do
     case $option in
-        l) list && exit 0 ;;
-        b) list | sed 's/--//g' && exit 0 ;;
+        l) list ; exit 0 ;;
+        b) list | sed 's/--//g' ; exit 0 ;;
         \?)
           printf "wrong option.\n"
           usage; exit 1
           ;;
-        h)
-          usage; exit 0
-          ;;
+        h) usage; exit 0 ;;
     esac
 done
 shift $(($OPTIND - 1))
