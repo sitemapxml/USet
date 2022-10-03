aac0e478918e9058be7f7ceb1d407bf734571b00
viktordw <viktor@dreamweb.rs>
2022-10-03 13:32:06 +0200

    Update arglist.inc.sh

--- includes/arglist.inc.sh
+++ includes/arglist.inc.sh
@@ -2,19 +2,19 @@ conf_skip_confirm=$(get_value "skip-confirm" "$@")
 
 conf_language=$(get_value "language" "$@")
 conf_disable_colors=$(get_value "disable-colors" "$@")
-conf_skip_welcome=$(get_value "skip-welcome" "$@")
+conf_skip_welcome=$(get_value "welcome" "$@")
 
 conf_debug=$(get_value "debug" "$@")
-conf_ignore_lockfile=$(get_value "ignore-lock" "$@")
-conf_create_lockfile=$(get_value "create-lock" "$@")
+conf_ignore_lockfile=$(get_value "lock-ignore" "$@")
+conf_create_lockfile=$(get_value "lock-create" "$@")
 
-conf_data_folder_name=$(get_value "data-folder-name" "$@")
-conf_data_file_name=$(get_value "data-file-name" "$@")
-conf_ssl_info_file_name=$(get_value "sslinfo-file-name" "$@")
-conf_db_info_file_name=$(get_value "dbinfo-file-name" "$@")
+conf_data_folder_name=$(get_value "data-dirname" "$@")
+conf_data_file_name=$(get_value "data-filename" "$@")
+conf_ssl_info_file_name=$(get_value "sslinfo-filename" "$@")
+conf_db_info_file_name=$(get_value "dbinfo-filename" "$@")
 
-conf_php_extension_list=$(get_value "install-php-extensions" "$@")
-conf_helper_program_list=$(get_value "install-programs" "$@")
+conf_php_extension_list=$(get_value "php-extensions" "$@")
+conf_helper_program_list=$(get_value "packages" "$@")
 
 conf_install_mysql=$(get_value "install-mysql" "$@")
 
@@ -28,7 +28,7 @@ conf_create_phpinfo=$(get_value "create-phpinfo" "$@")
 conf_install_adminer=$(get_value "install-adminer" "$@")
 conf_adminer_build=$(get_value "adminer-build" "$@")
 conf_enable_ufw=$(get_value "ufw-enable" "$@")
-conf_create_pass_backup=$(get_value "password-backup" "$@")
+conf_create_pass_backup=$(get_value "pass-backup" "$@")
 
 conf_hostname=$(get_value "hostname" "$@")
 conf_rootpass=$(get_value "rootpass" "$@")
@@ -39,8 +39,8 @@ conf_email=$(get_value "email" "$@")
 conf_http_server=$(get_value "web-server" "$@")
 
 conf_install_modphp=$(get_value "install-modphp" "$@")
-conf_disable_preinstall=$(get_value "disable-preinstall" "$@")
-conf_disable_postinstall=$(get_value "disable-postinstall" "$@")
+conf_disable_preinstall=$(get_value "preinstall-disable" "$@")
+conf_disable_postinstall=$(get_value "postinstall-disable" "$@")
 
 conf_ssl_install=$(get_value "ssl-install" "$@")
 conf_ssl_redirect=$(get_value "ssl-redirect" "$@")
